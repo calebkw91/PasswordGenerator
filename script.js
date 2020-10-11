d571ee8d4f9fc04c38170da3eeb173d0aeb5ee45
calebkw <walke1291@gmail.com>
2020-10-10 21:57:05 -0500

    Created array concats based on oassword parameters, created while loops to ensure password parameters are selected

--- script.js
+++ script.js
@@ -11,11 +11,35 @@ function writePassword() {
 }
 
 function generatePassword(){
-  let length = prompt("Choose a password length between 8-128");
-  let uppercase = confirm("Would you like uppercase letters?");
-  let lowercase = confirm("Would you like lowercase letters?");
-  let numbers = confirm("Would you like numbers?");
-  let specialCharacters = confirm("Would you like special characters?");
+
+  let passwordLengthSelected = true;
+  let passwordParameterSelected = true;
+  let finalPassword = "";
+  let uppercase = false;
+  let lowercase = false;
+  let numbers = false;
+  let specialCharacters = false;
+
+  while(passwordLengthSelected){
+    var length = prompt("Choose a password length between 8-128");
+
+    if (length > 7 && length < 129){
+      passwordLengthSelected = false;
+    }
+  }
+
+  while(passwordParameterSelected){
+    uppercase = confirm("Would you like uppercase letters?");
+    lowercase = confirm("Would you like lowercase letters?");
+    numbers = confirm("Would you like numbers?");
+    specialCharacters = confirm("Would you like special characters?");
+
+    if (uppercase || lowercase || numbers || specialCharacters){
+      passwordParameterSelected = false;
+    } else {
+      let retry = confirm("Please select at least 1 parameter.")
+    }
+  }
 
   let uppercaseChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   let lowercaseChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -40,7 +64,13 @@ function generatePassword(){
     passwordChars = passwordChars.concat(specialChars);
   } 
 
-  
+  for (i = 0; i < length; i++){
+    let passwordCharIndex = Math.floor(Math.random(i) * passwordChars.length)
+
+    finalPassword = finalPassword.concat(passwordChars[passwordCharIndex]);
+  }
+
+  return finalPassword;
 }
 
 // Add event listener to generate button
